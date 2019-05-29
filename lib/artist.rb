74e444a8e7366098f7d67d458579713e8773849a
Jason  Montes <jasone.montes@gmail.com>
2019-05-29 20:31:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,16 +20,16 @@ require_relative '../lib/artist.rb'
     @@artists
   end
 
-#   def add_song(song)
-#     @songs << song
-#     song.artist = self
-#   end
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
 
-#   def add_songs(songs)
-#     songs.each { |song| add_song(song) }
-#   end
+  def add_songs(songs)
+    songs.each { |song| add_song(song) }
+  end
 
-# end
+end
 
 
 
