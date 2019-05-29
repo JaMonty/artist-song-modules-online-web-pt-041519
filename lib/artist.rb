2a5d1b14cec0f12535cce501a4e82dfbebcb744f
Jason  Montes <jasone.montes@gmail.com>
2019-05-29 20:24:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,37 @@
 
 
-require_relative '../lib/artist.rb'
+# require_relative '../lib/artist.rb'
 
-  class Artist
-  attr_accessor :name
-  attr_reader :song
-  extend  Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  extend  Findable
-  include Paramable
+#   class Artist
+#   attr_accessor :name
+#   attr_reader :song
+#   extend  Memorable::ClassMethods
+#   include Memorable::InstanceMethods
+#   extend  Findable
+#   include Paramable
 
-  @@artists = []
+#   @@artists = []
 
-  def initialize
-    super
-    # self.class.all << self
-    @songs = []
-  end
+#   def initialize
+#     super
+#     # self.class.all << self
+#     @songs = []
+#   end
 
-  def self.all
-    @@artists
-  end
+#   def self.all
+#     @@artists
+#   end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
+#   def add_song(song)
+#     @songs << song
+#     song.artist = self
+#   end
 
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
+#   def add_songs(songs)
+#     songs.each { |song| add_song(song) }
+#   end
 
-end
+# end
 
 
 
