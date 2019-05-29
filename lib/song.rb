bed2128a22a779443d0b5d28d38958707bf1f17b
Jason  Montes <jasone.montes@gmail.com>
2019-05-29 20:25:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,26 +3,26 @@
 
 
 
-require_relative '../lib/song.rb'
+# require_relative '../lib/song.rb'
 
-  class Song
-  attr_accessor :name
-  attr_reader :artist
-  extend  Memorable::ClassMethods
-  include Memorable::InstanceMethods
-  extend  Findable
-  include Paramable
+#   class Song
+#   attr_accessor :name
+#   attr_reader :artist
+#   extend  Memorable::ClassMethods
+#   include Memorable::InstanceMethods
+#   extend  Findable
+#   include Paramable
 
-  @@songs = []
+#   @@songs = []
 
-  def artist=(artist)
-    @artist = artist
-  end
+#   def artist=(artist)
+#     @artist = artist
+#   end
 
-  def self.all
-    @@songs
-  end 
-end
+#   def self.all
+#     @@songs
+#   end 
+# end
 
 
 
