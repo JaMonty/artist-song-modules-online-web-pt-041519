1a0f4e5df44aaed5768f3746dba0fd6c2095e78b
Jason  Montes <jasone.montes@gmail.com>
2019-05-29 20:22:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 # require_relative '../lib/concerns/memorable.rb'
-require_relative '../lib/concerns/findable.rb'
+# require_relative '../lib/concerns/findable.rb'
 require_relative '../lib/concerns/paramable.rb'
 
 
